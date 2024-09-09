f3faf2d5c76c83c73bd3b0f8bebae6ea09195ef8
Mario Infante <mario.omar.infante@gmail.com>
2024-09-08 19:11:17 -0700

    IT WORKS!

--- homework/convolution-2d/kernel.cl
+++ homework/convolution-2d/kernel.cl
@@ -1,30 +1,55 @@
-
 __kernel void convolution2D(
-    __global float * inputData, __global float * outputData, __constant float * maskData,
-    int width, int height, int maskWidth,  int imageChannels){
+    __global float * inputData, 
+    __global float * outputData, 
+    __constant float * maskData,
+    int width, 
+    int height, 
+    int maskWidth,  
+    int imageChannels){
     //@@ Insert code to implement matrix multiplication here
 
-    /**
-    maskRadius := maskWidth/2 # this is integer division, so the result is 2
-    for i from 0 to height do
-    for j from 0 to width do
-        for k from 0 to channels
-        accum := 0
-        for y from -maskRadius to maskRadius do
-            for x from -maskRadius to maskRadius do
-            xOffset := j + x
-            yOffset := i + y
-            if xOffset >= 0 && xOffset < width &&
-                yOffset >= 0 && yOffset < height then
-                imagePixel := I[(yOffset * width + xOffset) * channels + k]
-                maskValue := K[(y+maskRadius)*maskWidth+x+maskRadius]
-                accum += imagePixel * maskValue
-            end
-            end
-        end
-        # pixels are in the range of 0 to 1
-        P[(i * width + j)*channels + k] = clamp(accum, 0, 1)
-        end
-    end
-    end */
+    // maskRadius := maskWidth/2 # this is integer division, so the result is 2
+    const int maskRadius = maskWidth / 2;
+
+    // for i from 0 to height do
+    int row = get_global_id(1);
+    
+    // for j from 0 to width do
+    int column = get_global_id(0);
+
+    // for k from 0 to channels
+    for (int k = 0; k < imageChannels; ++k) {
+
+        // accum := 0
+        float accum = 0.0f;
+
+        // for y from -maskRadius to maskRadius do
+        for (int y = -maskRadius; y <= maskRadius; ++y) {
+            // for x from -maskRadius to maskRadius do
+            for (int x = -maskRadius; x <= maskRadius; ++x) {
+                // xOffset := j + x
+                int xOffset = j + x;
+                // yOffset := i + y
+                int yOffset = i + y;
+
+                // if xOffset >= 0 && xOffset < width && yOffset >= 0 && yOffset < height then
+                if (xOffset >= 0 && xOffset < width && yOffset >= 0 && yOffset < height) {
+
+                    // imagePixel := I[(yOffset * width + xOffset) * channels + k]
+                    float imagePixel = inputData[(yOffset * width + xOffset) * imageChannels + k];
+
+                    // maskValue := K[(y+maskRadius)*maskWidth+x+maskRadius]
+                    float maskValue = maskData[(y + maskRadius) * maskWidth + (x + maskRadius)];
+
+                    // accum += imagePixel * maskValue
+                    accum += imagePixel * maskValue;
+                }
+            }
+        }
+
+        // min(max(x, lower), upper)
+        accum = fmin(fmax(accum, 0.0f), 1.0f);
+        // P[(i * width + j)*channels + k] = clamp(accum, 0, 1)
+        outputData[(i * width + j) * imageChannels + k] = accum;
+    }
 }
--- homework/convolution-2d/main.c
+++ homework/convolution-2d/main.c
@@ -15,15 +15,24 @@
 
 #define KERNEL_PATH "kernel.cl"
 
+    // Input0 = Image
+    // Input1 = Mask
+    // Result = Empty Matrix with image channels
 void OpenCLConvolution2D(Matrix *input0, Matrix *input1, Matrix *result)
 {
     // Load external OpenCL kernel code
     char *kernel_source = OclLoadKernel(KERNEL_PATH); // Load kernel source
 
+
+
     // Device input and output buffers
     cl_mem device_a, device_b, device_c;
 
+
+
     cl_int err;
+
+
 
     cl_device_id device_id;    // device ID
     cl_context context;        // context
@@ -31,39 +40,77 @@ void OpenCLConvolution2D(Matrix *input0, Matrix *input1, Matrix *result)
     cl_program program;        // program
     cl_kernel kernel;          // kernel
 
+
+
     // Find platforms and devices
     OclPlatformProp *platforms = NULL;
     cl_uint num_platforms;
 
+
+
     err = OclFindPlatforms((const OclPlatformProp **)&platforms, &num_platforms);
     CHECK_ERR(err, "OclFindPlatforms");
 
+
+
     // Get ID for first device on first platform
     device_id = platforms[0].devices[0].device_id;
+
+
 
     // Create a context
     context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
     CHECK_ERR(err, "clCreateContext");
 
+
+
     // Create a command queue
     queue = clCreateCommandQueueWithProperties(context, device_id, 0, &err);
     CHECK_ERR(err, "clCreateCommandQueueWithProperties");
+
+
 
     // Create the program from the source buffer
     program = clCreateProgramWithSource(context, 1, (const char **)&kernel_source, NULL, &err);
     CHECK_ERR(err, "clCreateProgramWithSource");
 
+
+
     // Build the program executable
     err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
     CHECK_ERR(err, "clBuildProgram");
+
+
 
     // Create the compute kernel in the program we wish to run
     kernel = clCreateKernel(program, "convolution2D", &err);
     CHECK_ERR(err, "clCreateKernel");
 
+
+
     //@@ Allocate GPU memory here
+    // Input0 = Image
+    // Input1 = Mask
+    // Result = Empty Matrix with image channels
+    device_a = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(float) * input0->shape[0] * input0->shape[1] * IMAGE_CHANNELS,NULL, &err);
+    CHECK_ERR(err, "clCreateBufferA");
+
+    device_b = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(float) * input1->shape[0] * input1->shape[1],NULL, &err);
+    CHECK_ERR(err, "clCreateBufferB");
+
+    device_c = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float) * result->shape[0] * result->shape[1] * IMAGE_CHANNELS,NULL, &err);
+    CHECK_ERR(err, "clCreateBufferC");
+
+
 
     //@@ Copy memory to the GPU here
+    err = clEnqueueWriteBuffer(queue, device_a, CL_TRUE, 0, sizeof(float) * input0->shape[0] * input0->shape[1] * IMAGE_CHANNELS, input0->data,0,NULL,NULL);
+    CHECK_ERR(err,"clEnqueueWriteBuffer");
+
+    err = clEnqueueWriteBuffer(queue, device_b, CL_TRUE, 0, sizeof(float) * input1->shape[0] * input1->shape[1], input1->data,0,NULL,NULL);
+    CHECK_ERR(err,"clEnqueueWriteBuffer");
+
+
 
     // Set the arguments to our compute kernel
     // __global float * inputData, __global float * outputData, __constant float * maskData,
@@ -84,13 +131,33 @@ void OpenCLConvolution2D(Matrix *input0, Matrix *input1, Matrix *result)
     err |= clSetKernelArg(kernel, 6, sizeof(unsigned int), &imageChannels);
     CHECK_ERR(err, "clSetKernelArg 6");
 
+
+
     // @@ define local and global work sizes
+    size_t global_item_size[2] = {input0->shape[1], input0->shape[0], IMAGE_CHANNELS};
+
+
 
     //@@ Launch the GPU Kernel here
+    err = clEnqueueNDRangeKernel(queue, kernel, 2, NULL, global_item_size, NULL, 0, NULL, NULL);
+    CHECK_ERR(err, "clEnqueueNDRangeKernel");
+
+
 
     //@@ Copy the GPU memory back to the CPU here
+    err = clEnqueueReadBuffer(queue, device_c, CL_TRUE, 0, result->shape[0] * result->shape[1] * sizeof(float) * IMAGE_CHANNELS, result->data, 0, NULL, NULL);
+    CHECK_ERR(err, "clEnqueueReadBuffer");
+
+
 
     //@@ Free the GPU memory here
+    clReleaseMemObject(device_a);
+    clReleaseMemObject(device_b);
+    clReleaseMemObject(device_c);
+    clReleaseProgram(program);
+    clReleaseKernel(kernel);
+    clReleaseCommandQueue(queue);
+    clReleaseContext(context);
 }
 
 int main(int argc, char *argv[])
@@ -107,28 +174,34 @@ int main(int argc, char *argv[])
     const char *input_file_d = argv[4];
 
     // Host input and output vectors and sizes
-    Matrix host_a, host_b, host_c, answer;
-    
     cl_int err;
-
+    Matrix host_a;
     err = LoadImg(input_file_a, &host_a);
     CHECK_ERR(err, "LoadImg");
 
+    Matrix host_b;
     err = LoadMatrix(input_file_b, &host_b);
     CHECK_ERR(err, "LoadMatrix");
 
+    Matrix answer;
     err = LoadImg(input_file_c, &answer);
     CHECK_ERR(err, "LoadImg");
 
     int rows, cols;
     //@@ Update these values for the output rows and cols of the output
     //@@ Do not use the results from the answer image
+    rows = host_a.shape[0];
+    cols = host_a.shape[1];
 
+    Matrix host_c;
     // Allocate the memory for the target.
     host_c.shape[0] = rows;
     host_c.shape[1] = cols;
     host_c.data = (float *)malloc(sizeof(float) * host_c.shape[0] * host_c.shape[1] * IMAGE_CHANNELS);
 
+    // Input0 = Image
+    // Input1 = Mask
+    // Result = Empty Matrix with image channels
     OpenCLConvolution2D(&host_a, &host_b, &host_c);
 
     // Save the image
